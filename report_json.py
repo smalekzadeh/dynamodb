ec1b3cee420b1e6d4736216d6a201f0385c9112e
Shahab Malekzadeh <you@example.com>
2016-12-13 15:34:27 +0000

    api request object cleanedup

--- report_json.py
+++ report_json.py
@@ -1,7 +1,7 @@
 {
    "reporttype":   "historic",  		 	# live|historic live returns the data from redis and historic from dynamo                                 
    "datefrom"  :   20161213123455 ,  		# int date with the "%Y%m%d%H%M%S" format (if not set no date sort will be used)
-   "dateto"	   :   20161214123455",  		# int date with the "%Y%m%d%H%M%S" format
+   "dateto"	   :   20161214123455,  		# int date with the "%Y%m%d%H%M%S" format
    "entitytype":   "account",        		# account|trunk|phonenumber   type of entity you want the report for
    "entityid"  :   "ACC-1230" ,       		# the id of chosen entity
    "filterby"  :   [{"calltype":"mobile"}], # list of filters which must be applied to the result set
